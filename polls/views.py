2bc493a3573eaaeb73380e19c03179f38aeff88f
yuehp <yuehp@hotmail.com>
2017-09-28 11:31:25 +0800

    part 6, done:customize app's look and feel

--- polls/views.py
+++ polls/views.py
@@ -14,11 +14,9 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
 
 class ResultsView(generic.DetailView):
     model = Question
